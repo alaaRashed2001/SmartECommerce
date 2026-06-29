a3151d40128817679d923156f2822d3638732faf
AlaaRashed <114382094+alaaRashed2001@users.noreply.github.com>
2026-06-29 12:57:07 +0300

    - initialize project folder structure, - app text, - and app colors

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import AppText from './src/components/text/AppText';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <AppText variant="medium">Open up App.tsx to start working on your app!</AppText>
+      <AppText variant="bold">Open up App.tsx to start working on your app!</AppText>
+  
     </View>
   );
 }
---
+++ src/styles/color.ts
@@ -0,0 +1,10 @@
+export const AppColors = {
+    primary: "#000",
+    white: "#fff",
+    black: "#000",
+    lightGrey: "#f0f0f0",
+    background: "#f6f6f6",
+    disabledGrey: "#D3D3D3",
+    blueGrey: "#E6E8EA",
+    medGrey: "#939393"
+}
