1194cbb5f4850b64ba72f156953232f5c3d24236
AlaaRashed <114382094+alaaRashed2001@users.noreply.github.com>
2026-07-01 12:17:40 +0300

    Style Bottom Tabs

--- src/navigation/MainAppBottomTaps.tsx
+++ src/navigation/MainAppBottomTaps.tsx
@@ -2,15 +2,51 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/home/HomeScreen';
 import CartScreen from '../screens/cart/CartScreen';
 import ProfileScreen from '../screens/profile/ProfileScreen';
+import { AppColors } from '../styles/color';
+import { s } from 'react-native-size-matters';
+import { vs } from 'react-native-size-matters';
+import { Ionicons } from '@expo/vector-icons'
+import { Is_Android } from '../constants/constants';
 
 const Tab = createBottomTabNavigator();
 
 export default function MainAppBottomTaps() {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Cart" component={CartScreen} />
-            <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Navigator
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: AppColors.primary,
+                tabBarLabelStyle: {
+                    marginTop: vs(4),
+                    fontSize: s(12),
+                },
+                tabBarStyle: Is_Android && {
+                    height: vs(50),
+                },
+            }}
+
+        >
+            <Tab.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="home" size={size} color={color} />
+                    ),
+                    title: 'Home'
+                }} />
+            <Tab.Screen name="Cart" component={CartScreen} options={{
+                tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="cart" size={size} color={color} />
+                ),
+                title: 'Cart'
+            }} />
+            <Tab.Screen name="Profile" component={ProfileScreen} options={{
+                tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="person" size={size} color={color} />
+                ),
+                title: 'Profile'
+            }} />
         </Tab.Navigator>
     );
 }
